275bb1277daeb05b10fa750af11cd4cad73a2eaf
Naoki Yoshimaru <nydsa529422@gmail.com>
2022-07-07 11:55:22 +0900

    create json File

--- src/main/resources/Scripts/googlemaps_api.py
+++ src/main/resources/Scripts/googlemaps_api.py
@@ -25,12 +25,12 @@ if len(args) <= 1:
 key =  args[1] # 上記で作成したAPIキーを入れる
 # コマンド：python googlemaps_api.py apiのkey
 
-client = googlemaps.Client(key) #インスタンス生成
-
 # 観光地の名前リストをCSVから抽出
-SightBasic_DF = pd.read_csv("data/20200731_日本科学未来館_SightBasic.csv")
+SightBasic_DF = pd.read_csv("../data/20200731_日本科学未来館_SightBasic.csv")
 name_ls = list(SightBasic_DF["SightName"])
 
+#インスタンス生成
+client = googlemaps.Client(key) 
 #================================================================================================================
 #geocodeデータを保存
 res_geocode_dict = {}
@@ -38,13 +38,13 @@ for name_i in name_ls:
     geocode_result = client.geocode(name_i,language='ja' ) # 観光地の位置情報を検索
     res_geocode_dict[name_i] = geocode_result
 #JSONとして出力
-GEOCODE_PATH = 'data/res_geocode.json'
+GEOCODE_PATH = '../data/res_geocode.json'
 save_json(res_geocode_dict,GEOCODE_PATH)
 
 #================================================================================================================
 #日本科学未来館の周辺情報を取り出す(JSONに出力)
 miraikan_result = client.geocode('日本科学未来館',language='ja' ) # 東京未来館の位置情報を検索
-MIRAIKAN_PATH = 'data/res_miraikan.json'
+MIRAIKAN_PATH = '../data/res_miraikan.json'
 save_json(miraikan_result,MIRAIKAN_PATH)
 #================================================================================================================
 #各地点の周辺情報を保存(保留)
@@ -59,19 +59,19 @@ for name_i in name_ls:
     place_informations = client.places(name_i,language='ja') # 観光地の位置情報を検索
     res_place_info_dict[name_i] = place_informations
 #JSONとして出力
-PLACE_INFO_PATH = 'data/res_place_info.json'
+PLACE_INFO_PATH = '../data/res_place_info.json'
 save_json(miraikan_result,PLACE_INFO_PATH)
 
 #================================================================================================================
 # 東京未来館から観光地まで移動方法・所要時間をを取得
 res_directions_info_dict = {}
-loc_miraikan = miraikan_result[0]["goemetry"]["location"] #日本未来館の緯度経度を取得
+loc_miraikan = miraikan_result[0]["geometry"]["location"] #日本未来館の緯度経度を取得
 
 for name_i in name_ls:
     trg_place_dict = res_geocode_dict[name_i]
-    trg_place_geocode = trg_place_dict[0]["goemetry"]["location"]
+    trg_place_geocode = trg_place_dict[0]["geometry"]["location"]
     place_directons= client.directions(origin=loc_miraikan, destination=trg_place_geocode,language= 'ja') 
     res_directions_info_dict[name_i] = place_directons
 #JSONとして出力
-PLACE_DIRECTIONS_PATH = 'data/res_directions_info.json'
+PLACE_DIRECTIONS_PATH = '../data/res_directions_info.json'
 save_json(res_directions_info_dict,PLACE_DIRECTIONS_PATH)
